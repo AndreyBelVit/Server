e6decd06de9c4b2c367ce81f29a1f46dfcc0fe32
Andrey L <jobitdev@gmail.com>
2023-10-05 20:20:02 +0300

    add dependencies and create class Frontend

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Frontend frontend = new Frontend();
+        Server server = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(frontend), "/authform");
+
+        try {
+            server.start();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            server.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
